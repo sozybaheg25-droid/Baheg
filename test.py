ec9a9c4dce356bce9465f0ddd4dafba1a5e11865
21now <168331277+21now@users.noreply.github.com>
2025-08-05 08:43:40 +0300

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-print("API_KEY:", os.getenv('API_KEY') and "***exists***")
-print("API_SECRET:", os.getenv('API_SECRET') and "***exists***")
-print("ACCESS_TOKEN:", os.getenv('ACCESS_TOKEN') and "***exists***")
-print("ACCESS_TOKEN_SECRET:", os.getenv('ACCESS_TOKEN_SECRET') and "***exists***")
